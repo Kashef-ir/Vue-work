041e0d9450a7153383813dd8dcb9655538626774
Mohamad Hashemi <muhamadhashemi142@gmail.com>
2025-04-22 10:32:58 +0330

    Refactor HomeVue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
-import HomeCards from "@/components/HomeCards.vue";
 import { navbarItems } from "@/data/navbarItems.js";
-import JobListings from "./components/JobListings.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <Navbar :items="navbarItems" />
-  <Hero
-    title="Become A Vue Dev"
-    subtitle="Find the Vue job that fits your skills and needs"
-  />
-  <HomeCards />
-  <JobListings :limit="3" :showButton="true"/>
+  <RouterView />
 </template>
